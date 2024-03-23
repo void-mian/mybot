95d98550b91abb6f8da8fb41a726979de5928742
int mian <voidmian@gmail.com>
2024-03-23 16:59:04 +0800

    refactor: filter pattern

--- bot/__main__.py
+++ bot/__main__.py
@@ -21,39 +21,56 @@ def get_bot(token: str, proxy_url: str | None):
     return builder.build()
 
 
-def read_simple(func: Callable[[int, int, str | None], str | None]):
+class Response(app.BaseResponse):
+    def __init__(self, chat_id: int, context: ContextTypes.DEFAULT_TYPE):
+        self.chat_id = chat_id
+        self.context = context
+        self.tasks = list()
+
+    def send(self, text: str):
+        self.tasks.append(self.context.bot.send_message(chat_id=self.chat_id, text=text))
+
+    def send_reply(self, chat_id: int, message_id: int, text: str):
+        self.tasks.append(self.context.bot.send_message(chat_id=chat_id,
+                                                        reply_to_message_id=message_id,
+                                                        text=text))
+
+
+def make_handler(func: Callable[[app.BaseRequest, app.BaseResponse], None]):
+    def get_request(update: Update):
+        reply_to_message = update.effective_message.reply_to_message
+        return app.BaseRequest(chat_id=update.effective_chat.id,
+                               message_id=update.effective_message.message_id,
+                               reply_to_message_id=None if reply_to_message is None else reply_to_message.message_id,
+                               text=update.effective_message.text)
+
     async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=func(update.effective_chat.id,
-                                                 update.effective_message.message_id,
-                                                 update.effective_message.text))
+        request = get_request(update)
+        response = Response(update.effective_chat.id, context)
+        func(request, response)
+        for task in response.tasks:
+            await task
 
     return handler
 
 
-def read_reply(func: Callable[[int, int, int, str | None], str | None]):
-    async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=func(update.effective_chat.id,
-                                                 update.effective_message.message_id,
-                                                 update.effective_message.reply_to_message.message_id,
-                                                 update.effective_message.text))
+def make_schedule(func: Callable[[app.BaseResponse], None]):
+    async def callback(context: ContextTypes.DEFAULT_TYPE):
+        response = Response((2 ** 64) - 1, context)
+        func(response)
+        for task in response.tasks:
+            await task
 
-    return handler
-
-
-async def reminder_checker(context: ContextTypes.DEFAULT_TYPE):
-    for chat_id, message_id, message in app.update_reminders():
-        await context.bot.send_message(chat_id=chat_id, reply_to_message_id=message_id, text=message)
+    return callback
 
 
 if __name__ == "__main__":
     setting()
     db.setup()
     bot = get_bot(config["telegram"]["token"], config.get("network", dict()).get("proxy"))
-    bot.add_handler(CommandHandler('start', read_simple(app.start)))
-    bot.add_handler(CommandHandler('shut_up', read_simple(app.shut_up)))
-    bot.add_handler(CommandHandler('reminder_monthly', read_simple(app.reminder_monthly)))
-    bot.add_handler(CommandHandler('cancel', read_reply(app.cancel)))
-    bot.job_queue.run_repeating(reminder_checker, 30)
+    bot.add_handler(CommandHandler('start', make_handler(app.start)))
+    bot.add_handler(CommandHandler('shut_up', make_handler(app.shut_up)))
+    bot.add_handler(CommandHandler('reminder_monthly', make_handler(app.reminder_monthly)))
+    bot.add_handler(CommandHandler('cancel', make_handler(app.cancel)))
+    bot.job_queue.run_repeating(make_schedule(app.check_reminders), 30)
     bot.run_polling()
--- bot/app.py
+++ bot/app.py
@@ -1,4 +1,5 @@
 from db import *
+import abc
 import logging
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -7,26 +8,44 @@ from zoneinfo import ZoneInfo
 import pickle
 
 
-def start(chat_id: int, message_id: int, text: str | None):
+class BaseRequest:
+    def __init__(self, chat_id: int, message_id: int | None, reply_to_message_id: int | None, text: str | None):
+        self.chat_id = chat_id
+        self.message_id = message_id
+        self.reply_to_message_id = reply_to_message_id
+        self.text = text
+
+
+class BaseResponse(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def send(self, text: str) -> None:
+        pass
+
+    @abc.abstractmethod
+    def send_reply(self, chat_id: int, message_id: int, text: str) -> None:
+        pass
+
+
+def start(req: BaseRequest, res: BaseResponse):
+    chat_id = req.chat_id
     chat, created = Chat.get_or_create(chat_id=str(chat_id))
     if not chat.enabled:
         chat.enabled = True
         chat.save()
         logging.info(f"[{chat_id}] chat enabled")
-        return "我们已经是好友啦，一起来聊天吧😅！"
+        res.send("我们已经是好友啦，一起来聊天吧😅！")
     else:
-        return "直接聊天吧"
+        res.send("直接聊天吧")
 
 
-def shut_up(chat_id: int, message_id: int, text: str | None):
+def shut_up(req: BaseRequest, res: BaseResponse):
+    chat_id = req.chat_id
     chat, created = Chat.get_or_create(chat_id=str(chat_id))
     if chat.enabled:
         chat.enabled = False
         chat.save()
         logging.info(f"[{chat_id}] chat disabled")
-        return "I can't breathe🤐!"
-    else:
-        return None
+        res.send("I can't breathe🤐!")
 
 
 class ReminderInfo:
@@ -36,10 +55,9 @@ class ReminderInfo:
         self.count = count
 
 
-def update_reminders():
+def check_reminders(res: BaseResponse):
     now = datetime.now(ZoneInfo("UTC"))
     logging.debug("update reminders:")
-    reply = list()
     for reminder in Reminder.select():
         chat_id = int(reminder.chat.chat_id)
         message_id = int(reminder.message_id)
@@ -53,26 +71,28 @@ def update_reminders():
             Reminder.update(info=pickle.dumps(info)).where(
                 (Reminder.chat == str(chat_id)) & (Reminder.message_id == str(message_id))
             ).execute()
-            reply.append((chat_id, message_id, " 到点了!"))
-    return reply
+            res.send_reply(chat_id, message_id, " 到点了!")
 
 
-def reminder_monthly(chat_id: int, message_id: int, dt: str | None) -> str | None:
+def reminder_monthly(req: BaseRequest, res: BaseResponse):
+    chat_id = req.chat_id
+    message_id = req.message_id
+    dt = req.text
     chat, created = Chat.get_or_create(chat_id=str(chat_id))
     if not chat.enabled:
-        return None
+        return
 
     try:
         if dt:
             dtime = dateutil.parser.parse(timestr=dt, fuzzy=True)
             if dtime is None or dtime.tzinfo is None:
-                return "日期格式错误"
+                return res.send("日期格式错误")
         else:
-            return "日期格式错误"
+            return res.send("日期格式错误")
     except (dateutil.parser.ParserError, OverflowError) as error:
-        return "日期格式错误"
+        return res.send("日期格式错误")
 
-    cancel(chat_id=chat_id, message_id=None, reply_to_message_id=message_id)
+    canceled = cancel0(chat_id=chat_id, message_id=message_id)
 
     Reminder.create(
         chat=str(chat_id), message_id=str(message_id),
@@ -81,21 +101,26 @@ def reminder_monthly(chat_id: int, message_id: int, dt: str | None) -> str | Non
     dtime_str = dtime.strftime('%Y-%m-%d %H:%M:%S %Z')
     logging.info(f"[{chat_id},{message_id}] reminder added: {dtime_str}")
 
-    if cancel == "取消成功":
-        return f"替换成功 {dtime_str}"
+    if canceled == "取消成功":
+        res.send_reply(chat_id, message_id, f"替换成功 {dtime_str}")
     else:
-        return f"添加成功 {dtime_str}"
+        res.send_reply(chat_id, message_id, f"添加成功 {dtime_str}")
 
 
-def cancel(chat_id: int, message_id: int | None, reply_to_message_id: int, text: str | None = None) -> str | None:
-    chat, created = Chat.get_or_create(chat_id=str(chat_id))
-    if not chat.enabled:
-        return None
-
+def cancel0(chat_id: int, message_id: int) -> str:
     deleted = Reminder.delete().where(
-        (Reminder.chat == str(chat_id)) & (Reminder.message_id == str(reply_to_message_id))
+        (Reminder.chat == str(chat_id)) & (Reminder.message_id == str(message_id))
     ).execute()
     if deleted > 0:
         return "取消成功"
     else:
         return "没有任务"
+
+
+def cancel(req: BaseRequest, res: BaseResponse):
+    chat_id = req.chat_id
+    reply_to_message_id = req.reply_to_message_id
+    chat, created = Chat.get_or_create(chat_id=str(chat_id))
+    if not chat.enabled:
+        return None
+    res.send(cancel0(chat_id, reply_to_message_id))
